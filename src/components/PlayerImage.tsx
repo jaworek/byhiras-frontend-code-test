c1054c17e5b50c5d388608c7435d51247309190f
Jan Jaworski <jan.jaworski@portacapena.com>
2020-08-23 21:59:03 +0200

    Update PlayerImage

--- src/components/PlayerImage.tsx
+++ src/components/PlayerImage.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-function PlayerImage() {
-  return <div>Image</div>;
+type Props = {
+  image: string;
+};
+
+function PlayerImage({ image }: Props) {
+  return (
+    <img
+      className="flex flex-col w-20 h-30"
+      src={image}
+      width={180}
+      height={360}
+      alt="avatar"
+    />
+  );
 }
 
 export default PlayerImage;
