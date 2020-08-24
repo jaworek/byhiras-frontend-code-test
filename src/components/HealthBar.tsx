4ca6b910d74bb3bbdf3de4c05a4bec041125f0e2
Jan Jaworski <jan.jaworski@portacapena.com>
2020-08-24 08:26:32 +0200

    Update health bar

--- src/components/HealthBar.tsx
+++ src/components/HealthBar.tsx
@@ -5,37 +5,52 @@ type Props = {
   resolveGame: () => void;
 };
 
-function HealthBar({ health }: Props) {
-  let barColor = "bg-green-600";
+function getBarColor(health: number) {
   if (health === 100) {
-    barColor = "bg-green-600";
-  } else if (health > 90) {
-    barColor = "bg-green-500";
-  } else if (health > 80) {
-    barColor = "bg-green-400";
-  } else if (health > 70) {
-    barColor = "bg-yellow-400";
-  } else if (health > 60) {
-    barColor = "bg-yellow-500";
-  } else if (health > 50) {
-    barColor = "bg-yellow-600";
-  } else if (health > 40) {
-    barColor = "bg-yellow-700";
-  } else if (health > 30) {
-    barColor = "bg-red-500";
-  } else if (health > 20) {
-    barColor = "bg-red-600";
-  } else if (health > 10) {
-    barColor = "bg-red-700";
-  } else if (health > 0) {
-    barColor = "bg-red-800";
+    return "bg-green-600";
+  }
+  if (health > 90) {
+    return "bg-green-500";
+  }
+  if (health > 80) {
+    return "bg-green-400";
+  }
+  if (health > 70) {
+    return "bg-yellow-400";
+  }
+  if (health > 60) {
+    return "bg-yellow-500";
+  }
+  if (health > 50) {
+    return "bg-yellow-600";
+  }
+  if (health > 40) {
+    return "bg-yellow-700";
+  }
+  if (health > 30) {
+    return "bg-red-500";
+  }
+  if (health > 20) {
+    return "bg-red-600";
+  }
+  if (health > 10) {
+    return "bg-red-700";
+  }
+  if (health > 0) {
+    return "bg-red-800";
   }
 
+  return "bg-green-600";
+}
+
+function HealthBar({ health }: Props) {
   return (
     <div className="flex flex-col items-center">
       <div className="w-6 h-40 bg-gray-400 flex flex-col justify-end">
         <div
-          className={`transition-all duration-500 ease-in-out ${barColor}`}
+          className={`transition-all duration-500 ease-in-out ${getBarColor(
+            health
+          )}`}
           style={{ height: `${health > 0 ? health : 0}%` }}
         />
       </div>
